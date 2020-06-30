13aa0dd05f030bd2f42325d7c3aab8a65bb2d6be
Tom Marti <42736938+TomMarti@users.noreply.github.com>
2020-06-30 16:23:21 +0200

    initiale commit

---
+++ src/flux/Flux.py
@@ -0,0 +1,13 @@
+class Flux:
+    def __init__(self, hertz=44100):
+        self.url = ''
+        self.hertz = hertz
+
+    def connect(self, url):
+        pass
+
+    def upload(self, file):
+        pass
+
+    def download(self, file):
+        pass
